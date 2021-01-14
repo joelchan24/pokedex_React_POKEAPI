d75cafe5dfa1355c6fcf557259049932be2e5eed
JoelAldair <joelchan122@gmail.com>
2021-01-13 21:08:20 -0600

    im uploading my project in my repository

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
+# Hi Folk 
+
+Thanks for viewing my repository, Joel
 # Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
--- src/App.js
+++ src/App.js
@@ -1,23 +1,94 @@
 import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
+import { useEffect, useState } from 'react';
 
-function App() {
+const App = () => {
+  const [pokemon, setPokemon] = useState("pikachu");
+  const [pokemonData, setPokemonData] = useState([]);
+  const [pokemonType, setPOkemonType] = useState("");
+
+  const handleChange = (e) => {
+    setPokemon(e.target.value.toLowerCase());
+
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    
+    getPokemon();
+
+  };
+  const getPokemon = async () => {
+    const toArray = [];
+    try {
+
+
+      const url = `https://pokeapi.co/api/v2/pokemon/${pokemon}`;
+      const res = await axios.get(url);
+      console.log(res.data)
+      toArray.push(res.data);
+      setPOkemonType(res.data.types[0].type.name);
+      setPokemonData(toArray);
+      //console.log(res);
+    } catch (e) {
+
+      console.log(e);
+    }
+  };
+
+ 
+
+
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleSubmit}>
+        <label>
+          <input type="text" 
+          onChange={handleChange}
+           placeholder="Enter Pokemon Name" />
+        </label>
+      </form>
+      {pokemonData.map((data) => {
+        return (
+          <div className="container">
+            <img src={data.sprites["front_default"]} />
+            <div className="divTable" >
+              <div className="divTableBody" >
+                <div className="divTableRow">
+                  <div className="divTableCell">Type </div>
+                  <div className="divTableCell">{pokemonType} </div>
+                </div>
+                <div className="divTableRow">
+                  <div className="divTableCell">Height </div>
+                  <div className="divTableCell">
+                  {" "}
+                  {Math.round(data.height * 3.9)}"
+                  </div>
+                </div>
+                <div className="divTableRow">
+                  <div className="divTableCell">Weight </div>
+                  <div className="divTableCell">{" "}
+                  {Math.round(data.weight/4.3)} lbs   
+                 </div>
+                </div>
+                <div className="divTableRow">
+                  <div className="divTableCell">Number of Battles </div>
+                  <div className="divTableCell">
+                  {data.game_indices.length} 
+                 
+                 </div>
+                </div>
+              </div>
+            </div>
+
+          </div>
+
+        )
+      })}
     </div>
   );
 }
